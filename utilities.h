b0883e6743fe9d695df552e63fa78f4cfac9363d
Iman Alavi Fazel <im.alavifazel@gmail.com>
2019-01-17 19:52:06 +0330

    changed an int to size_t

--- utilities.h
+++ utilities.h
@@ -79,7 +79,7 @@ namespace afutils {
       std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) 
       { 
 	//os << "["; 
-	for (int i = 0; i < v.size(); ++i) { 
+	for (size_t i = 0; i < v.size(); ++i) { 
 	  os << v[i]; 
 	  if (i != v.size() - 1) 
             os << "  "; 
