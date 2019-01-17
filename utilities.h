4c5b23907782f850553703d912943b23420cd5af
Iman Alavi Fazel <im.alavifazel@gmail.com>
2019-01-17 19:38:40 +0330

    added overloaded operators for printing vectors

--- utilities.h
+++ utilities.h
@@ -12,11 +12,17 @@
 #include <cstring>
 #include <string>
 #include <bitset>
+#include <iostream>
 
-#include <boost/algorithm/string/classification.hpp>
-#include <boost/algorithm/string/split.hpp>
+#ifdef BOOST_VERSION
+#include <boost/algorithm/string/classification.hpp> */
+#include <boost/algorithm/string/split.hpp> */
+#endif
 
-namespace afutil {
+#define DUMP(a)								\
+    do { std::cout << #a " is value " << (a) << std::endl; } while(false)
+
+namespace afutils {
     inline std::string executeCommand(const char *cmd)
     {
         std::array<char, 128> buffer;
@@ -33,12 +39,16 @@ namespace afutil {
         return result;
     }
 
+#ifdef BOOST_VERSION
+
     inline std::vector<std::string> textSplit(std::string s, std::string b)
     {
       std::vector<std::string> parts;
       boost::split(parts, s, boost::is_any_of(b), boost::token_compress_on);
       return parts;
     }
+
+#endif
 
     inline void sleep(int x){
       std::this_thread::sleep_for(std::chrono::milliseconds(x));
@@ -53,7 +63,7 @@ namespace afutil {
 
     inline std::string string_to_hex(const std::string input)
     {
-        static const string t = "0123456789ABCDEF";
+      static const std::string t = "0123456789ABCDEF";
         std::string output;
         for (size_t i = 0; i < input.size(); i++) {
             const unsigned char c = input[i];
@@ -63,6 +73,59 @@ namespace afutil {
         }
         return output;
     }
+
+
+    template <typename T> 
+      std::ostream& operator<<(std::ostream& os, const std::vector<T>& v) 
+      { 
+	//os << "["; 
+	for (int i = 0; i < v.size(); ++i) { 
+	  os << v[i]; 
+	  if (i != v.size() - 1) 
+            os << "  "; 
+	} 
+	os << '\n'; // could be "]\n"
+	os << '\n';
+	return os; 
+      }
+
+    template <typename T> 
+      std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<T>>& v) 
+      {
+	// os << '[';
+	for (size_t i = 0; i < v.size(); ++i){
+	  // os << "["; 
+	  for (size_t j = 0; j < v[i].size(); ++j) { 
+	    os << v[i][j];
+	    if (j != v[i].size() - 1) 
+	      os << "  "; 
+	  } 
+	  os << '\n';
+	}
+	os << '\n';
+	return os; 
+      }
+
+    template <typename T> 
+      std::ostream& operator<<(std::ostream& os, const std::vector<std::vector<std::vector<T>>>& v) 
+      {
+	for (size_t i = 0; i < v.size(); ++i){
+	  // os << '[';
+	  for (size_t j = 0; j < v[i].size(); ++j){
+	    // os << "["; 
+	    for (size_t k = 0; k < v[i][j].size(); ++k) { 
+	      os << v[i][j][k];
+	      if (k != v[i][j].size() - 1) 
+		os << "  "; 
+	    } 
+	    os << '\n';
+	  }
+	  os << '\n';
+	}	     
+	return os; 
+      }
+
 }
+      
 
 #endif
